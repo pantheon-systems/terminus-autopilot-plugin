f3ae427ed9d9a88b2678845d4672dd387bbcb0d5
Sergei Churilo <sergei.churilo@pantheon.io>
2022-10-14 12:30:31 +0300

    Print autopilot setting in autopilot:syncing command

--- src/Commands/AutopilotEnvSyncingCommand.php
+++ src/Commands/AutopilotEnvSyncingCommand.php
@@ -3,19 +3,33 @@
 namespace Pantheon\TerminusAutopilot\Commands;
 
 use Pantheon\Terminus\Commands\TerminusCommand;
+use Pantheon\Terminus\Request\RequestAwareInterface;
+use Pantheon\Terminus\Site\SiteAwareInterface;
+use Pantheon\Terminus\Site\SiteAwareTrait;
+use Pantheon\TerminusAutopilot\AutopilotApi\AutopilotClientAwareTrait;
 
 /**
  * Class AutopilotEnvSyncingCommand.
  */
-class AutopilotEnvSyncingCommand extends TerminusCommand
+class AutopilotEnvSyncingCommand extends TerminusCommand implements RequestAwareInterface, SiteAwareInterface
 {
+    use AutopilotClientAwareTrait;
+    use SiteAwareTrait;
+
     /**
      * Command to enable/disable environment syncing.
      *
      * @command autopilot:syncing
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Pantheon\Terminus\Exceptions\TerminusException
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
      */
-    public function envSyncing()
+    public function envSyncing(string $site_id)
     {
-        $this->log()->notice('env syncing');
+        $site = $this->getSite($site_id);
+        $settings = $this->getClient()->getSettings($site->id);
+        print_r($settings);
     }
 }
