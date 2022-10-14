a91c6ae2c4e84183cfe416d4460aac19f8568a8c
Sergei Churilo <sergei.churilo@pantheon.io>
2022-10-14 12:08:45 +0300

    Add AutopilotClientAwareTrait.php

---
+++ src/AutopilotApi/AutopilotClientAwareTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Pantheon\TerminusAutopilot\AutopilotApi;
+
+use Pantheon\Terminus\Request\RequestAwareTrait;
+
+/**
+ * Class AutopilotClientAwareTrait.
+ *
+ * @package \Pantheon\TerminusAutopilot\AutopilotApi
+ */
+trait AutopilotClientAwareTrait
+{
+    use RequestAwareTrait;
+
+    /**
+     * @var \Pantheon\TerminusAutopilot\AutopilotApi\Client
+     */
+    protected $autopilotClient;
+
+    /**
+     * Return the AutopilotApi object.
+     *
+     * @return \Pantheon\TerminusAutopilot\AutopilotApi\Client
+     */
+    public function getClient(): Client
+    {
+        if ($this->autopilotClient) {
+            return $this->autopilotClient;
+        }
+
+        return $this->autopilotClient = new Client($this->request());
+    }
+}
