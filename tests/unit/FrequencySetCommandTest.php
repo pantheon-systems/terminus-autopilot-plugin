455c037028b0c4875d6e93f18062dfdb6b1ae041
Sergei Churilo <sergei.churilo@pantheon.io>
2022-10-17 11:25:15 +0300

    Add FrequencySetCommandTest

---
+++ tests/unit/FrequencySetCommandTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Pantheon\TerminusHello\Model;
+
+use PHPUnit\Framework\TestCase;
+
+final class FrequencySetCommandTest extends TestCase
+{
+    public function testFrequencySetCommand()
+    {
+        $this->markTestSkipped();
+    }
+}
