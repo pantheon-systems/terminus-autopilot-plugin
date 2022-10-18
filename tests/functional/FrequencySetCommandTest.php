0a31454e02f44995be456be50882003077964389
Sergei Churilo <sergei.churilo@pantheon.io>
2022-10-18 15:06:14 +0300

    Update testFrequencySetCommand()

--- tests/functional/FrequencySetCommandTest.php
+++ tests/functional/FrequencySetCommandTest.php
@@ -17,19 +17,43 @@ final class FrequencySetCommandTest extends TerminusTestBase
             'data' => ['updateFrequency' => 'WEEKLY'],
             'status_code' => 200,
         ]);
-        $result = $this->terminus(sprintf('site:autopilot:frequency %s', $this->getSiteName()), []);
-        $this->assertStringContainsString('weekly', $result);
+
+        $output = $this->terminus(sprintf('site:autopilot:frequency %s', $this->getSiteName()));
+        $this->assertEquals('weekly', $output);
+
+        $output = $this->terminus(sprintf('site:autopilot:frequency %s daily', $this->getSiteName()), ['2>&1']);
+        $this->assertStringContainsString('Autopilot frequency updated to daily.', $output);
+
+        $output = $this->terminus(
+            sprintf('site:autopilot:frequency %s invalid_frequency', $this->getSiteName()),
+            ['2>&1']
+        );
+        $this->assertStringContainsString(
+            'Autopilot frequency did not successfully update: "invalid_frequency" is not a valid frequency value',
+            $output
+        );
 
         $this->setMockPayload([
             'data' => null,
             'status_code' => 404,
-            'status_code_reason' => 'not found'
+            'status_code_reason' => 'not found',
         ]);
-        $result = $this->terminus(
+
+        $output = $this->terminus(
             sprintf('site:autopilot:frequency %s', $this->getSiteName()),
             ['2>&1'],
             false
         );
-        $this->assertStringContainsString('Failed requesting Autopilot API: not found', $result);
+        $this->assertStringContainsString('Failed requesting Autopilot API: not found', $output);
+
+        $output = $this->terminus(
+            sprintf('site:autopilot:frequency %s manual', $this->getSiteName()),
+            ['2>&1'],
+            false
+        );
+        $this->assertStringContainsString(
+            'Autopilot frequency did not successfully update: Failed requesting Autopilot API: not found',
+            $output
+        );
     }
 }
