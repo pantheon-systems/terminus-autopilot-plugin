a64c1f7b770f6de7a49d926952eae319ea2c08b0
Sergei Churilo <sergei.churilo@pantheon.io>
2022-10-18 11:13:53 +0300

    Add assertion

--- tests/functional/FrequencySetCommandTest.php
+++ tests/functional/FrequencySetCommandTest.php
@@ -3,12 +3,11 @@
 namespace Pantheon\TerminusHello\Model;
 
 use Pantheon\Terminus\Tests\Functional\TerminusTestBase;
-use PHPUnit\Framework\TestCase;
 
 final class FrequencySetCommandTest extends TerminusTestBase
 {
     public function testFrequencySetCommand()
     {
-        $this->markTestSkipped();
+        $this->assertCommandExists('site:autopilot:env-sync:enable');
     }
 }
