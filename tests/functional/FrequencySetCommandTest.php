10a789f92d70b813e7b253bb1aaa1b6fde1b9bec
Sergei Churilo <sergei.churilo@pantheon.io>
2022-10-19 11:46:27 +0300

    Add testing scenario for site:autopilot:frequency command

--- tests/functional/FrequencySetCommandTest.php
+++ tests/functional/FrequencySetCommandTest.php
@@ -3,11 +3,21 @@
 namespace Pantheon\TerminusAutopilot\Tests\Functional;
 
 use Pantheon\Terminus\Tests\Functional\TerminusTestBase;
+use Pantheon\TerminusAutopilot\Tests\Functional\Mocks\MockPayloadAwareTrait;
 
 final class FrequencySetCommandTest extends TerminusTestBase
 {
+    use MockPayloadAwareTrait;
+
     public function testFrequencySetCommand()
     {
         $this->assertCommandExists('site:autopilot:frequency');
+
+        $this->setMockPayload([
+            'data' => ['updateFrequency' => 'WEEKLY'],
+            'status_code' => 200,
+        ]);
+        $output = $this->terminus(sprintf('site:autopilot:frequency %s', $this->getSiteName()));
+        $this->assertEquals('weekly', $output);
     }
 }
