2d6c50dc785a7c9b4ee93b3d4e669bc51389507e
Sergei Churilo <sergei.churilo@pantheon.io>
2022-10-19 16:04:04 +0300

    Add testing scenario for site:autopilot:frequency command

--- tests/functional/FrequencyCommandTest.php
+++ tests/functional/FrequencyCommandTest.php
@@ -48,5 +48,19 @@ final class FrequencyCommandTest extends TerminusTestBase
             'Autopilot frequency did not successfully update: "invalid_frequency" is not a valid frequency value',
             $output
         );
+
+        $this->setMockPayload([
+            'data' => null,
+            'status_code' => 500,
+            'status_code_reason' => 'server error',
+        ]);
+
+        // Get "frequency" setting value for non-200 status from API.
+        $output = $this->terminus(
+            sprintf('site:autopilot:frequency %s', $this->getSiteName()),
+            ['2>&1'],
+            false
+        );
+        $this->assertStringContainsString('Failed requesting Autopilot API: server error', $output);
     }
 }
