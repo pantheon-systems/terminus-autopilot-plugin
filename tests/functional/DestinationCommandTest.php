5e8264a03ab31130017a4366d25c5d551a477a96
Sergei Churilo <sergei.churilo@pantheon.io>
2022-10-20 11:28:07 +0300

    Add testing scenario for site:autopilot:destination command

--- tests/functional/DestinationCommandTest.php
+++ tests/functional/DestinationCommandTest.php
@@ -20,6 +20,7 @@ final class DestinationCommandTest extends TerminusTestBase
      * @covers \Pantheon\TerminusAutopilot\Commands\DestinationCommand::destination()
      *
      * @see \Pantheon\TerminusAutopilot\AutopilotApi\Client::getDestination()
+     * @see \Pantheon\TerminusAutopilot\AutopilotApi\Client::setDestination()
      * @see \Pantheon\TerminusAutopilot\Tests\Functional\Mocks\RequestMock::request()
      */
     public function testDestinationSetCommand()
@@ -34,5 +35,9 @@ final class DestinationCommandTest extends TerminusTestBase
         // Get "destination" setting value.
         $output = $this->terminus(sprintf('site:autopilot:destination %s', $this->getSiteName()));
         $this->assertEquals('dev', $output);
+
+        // Set a valid "destination" setting value.
+        $output = $this->terminus(sprintf('site:autopilot:destination %s test', $this->getSiteName()), ['2>&1']);
+        $this->assertStringContainsString('Autopilot destination updated to test.', $output);
     }
 }
