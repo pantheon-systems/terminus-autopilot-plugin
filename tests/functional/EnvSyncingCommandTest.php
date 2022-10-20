c68e7e5866ccbed2f53dc6329289adddc5f82aa6
Sergei Churilo <sergei.churilo@pantheon.io>
2022-10-20 11:55:43 +0300

    Add testing scenario for site:autopilot:env-sync:enable and  site:autopilot:env-sync:disable commands

--- tests/functional/EnvSyncingCommandTest.php
+++ tests/functional/EnvSyncingCommandTest.php
@@ -40,5 +40,27 @@ final class EnvSyncingCommandTest extends TerminusTestBase
         // Disable environment syncing.
         $output = $this->terminus(sprintf('site:autopilot:env-sync:disable %s', $this->getSiteName()), ['2>&1']);
         $this->assertStringContainsString('Autopilot environment syncing is disabled.', $output);
+
+        // Run 'site:autopilot:env-sync:enable' command for a non-existing site.
+        $non_existing_site_name = 'some-non-existing-site-12345';
+        $output = $this->terminus(
+            sprintf('site:autopilot:env-sync:enable %s', $non_existing_site_name),
+            ['2>&1'],
+            false
+        );
+        $this->assertStringContainsString(
+            sprintf('Could not locate a site your user may access identified by %s', $non_existing_site_name),
+            $output
+        );
+        // Run 'site:autopilot:env-sync:disable' command for a non-existing site.
+        $output = $this->terminus(
+            sprintf('site:autopilot:env-sync:disable %s', $non_existing_site_name),
+            ['2>&1'],
+            false
+        );
+        $this->assertStringContainsString(
+            sprintf('Could not locate a site your user may access identified by %s', $non_existing_site_name),
+            $output
+        );
     }
 }
