3e339329923883865cc2f322e5a32f858b6f2ef7
Sergei Churilo <sergei.churilo@pantheon.io>
2022-10-21 10:33:53 +0300

    Add testing scenario for site:autopilot:deactivate command

--- tests/functional/DeactivateCommandTest.php
+++ tests/functional/DeactivateCommandTest.php
@@ -3,6 +3,7 @@
 namespace Pantheon\TerminusAutopilot\Tests\Functional;
 
 use Pantheon\Terminus\Tests\Functional\TerminusTestBase;
+use Pantheon\TerminusAutopilot\Tests\Functional\Mocks\MockPayloadAwareTrait;
 
 /**
  * Class DeactivateCommandTest.
@@ -11,13 +12,23 @@ use Pantheon\Terminus\Tests\Functional\TerminusTestBase;
  */
 final class DeactivateCommandTest extends TerminusTestBase
 {
+    use MockPayloadAwareTrait;
+
     /**
      * @test
      *
      * @covers \Pantheon\TerminusAutopilot\Commands\DeactivateCommand::deactivate()
+     *
+     * @see \Pantheon\TerminusAutopilot\AutopilotApi\Client::deactivate()
+     * @see \Pantheon\TerminusAutopilot\Tests\Functional\Mocks\RequestMock::request()
      */
     public function testDeactivateCommand()
     {
         $this->assertCommandExists('site:autopilot:deactivate');
+
+        $this->setMockPayload([]);
+
+        $output = $this->terminus(sprintf('site:autopilot:deactivate %s', $this->getSiteName()), ['2>&1']);
+        $this->assertStringContainsString('Autopilot is deactivated.', $output);
     }
 }
