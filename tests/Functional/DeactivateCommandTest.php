94e781ecc91c7d9aa7aaad9262dc8a03259643bc
Sergei Churilo <sergei.churilo@pantheon.io>
2022-10-21 13:19:33 +0300

    Add testing scenario for site:autopilot:deactivate command

--- tests/Functional/DeactivateCommandTest.php
+++ tests/Functional/DeactivateCommandTest.php
@@ -29,5 +29,17 @@ final class DeactivateCommandTest extends CommandTestBase
 
         $output = $this->terminus(sprintf('site:autopilot:deactivate %s', $this->getSiteName()), ['2>&1']);
         $this->assertStringContainsString('Autopilot is deactivated.', $output);
+
+        // Run the command for a non-existing site.
+        $non_existing_site_name = 'some-non-existing-site-12345';
+        $output = $this->terminus(
+            sprintf('site:autopilot:deactivate %s', $non_existing_site_name),
+            ['2>&1'],
+            false
+        );
+        $this->assertStringContainsString(
+            sprintf('Could not locate a site your user may access identified by %s', $non_existing_site_name),
+            $output
+        );
     }
 }
