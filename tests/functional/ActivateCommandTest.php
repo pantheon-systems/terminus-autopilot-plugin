27c566b5712ef67081043843ea4ab21bcbb47b59
Sergei Churilo <sergei.churilo@pantheon.io>
2022-10-21 10:16:48 +0300

    Add testing scenario for site:autopilot:activate command

--- tests/functional/ActivateCommandTest.php
+++ tests/functional/ActivateCommandTest.php
@@ -3,6 +3,7 @@
 namespace Pantheon\TerminusAutopilot\Tests\Functional;
 
 use Pantheon\Terminus\Tests\Functional\TerminusTestBase;
+use Pantheon\TerminusAutopilot\Tests\Functional\Mocks\MockPayloadAwareTrait;
 
 /**
  * Class ActivateCommandTest.
@@ -11,13 +12,23 @@ use Pantheon\Terminus\Tests\Functional\TerminusTestBase;
  */
 final class ActivateCommandTest extends TerminusTestBase
 {
+    use MockPayloadAwareTrait;
+
     /**
      * @test
      *
      * @covers \Pantheon\TerminusAutopilot\Commands\ActivateCommand::activate()
+     *
+     * @see \Pantheon\TerminusAutopilot\AutopilotApi\Client::activate()
+     * @see \Pantheon\TerminusAutopilot\Tests\Functional\Mocks\RequestMock::request()
      */
     public function testActivateCommand()
     {
         $this->assertCommandExists('site:autopilot:activate');
+
+        $this->setMockPayload([]);
+
+        $output = $this->terminus(sprintf('site:autopilot:activate %s', $this->getSiteName()), ['2>&1']);
+        $this->assertStringContainsString('Autopilot is activated.', $output);
     }
 }
