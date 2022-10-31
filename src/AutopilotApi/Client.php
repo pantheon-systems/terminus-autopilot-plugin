9821ac55bdfba27aaebcaa91cd4aec8fb34a0282
Phil Tyler <phil.tyler@pantheon.io>
2022-10-31 13:07:30 -0700

    [OTTO-2898] Update Autopilot Initialize/Delete Endpoints

--- src/AutopilotApi/Client.php
+++ src/AutopilotApi/Client.php
@@ -55,7 +55,7 @@ class Client
         ];
 
         $this->requestApi(
-            sprintf('sites/%s/vrt/initialize', $site_id),
+            sprintf('sites/%s', $site_id),
             $request_options
         );
     }
@@ -75,7 +75,7 @@ class Client
         ];
 
         $this->requestApi(
-            sprintf('sites/%s/vrt/terminate', $site_id),
+            sprintf('sites/%s', $site_id),
             $request_options
         );
     }
