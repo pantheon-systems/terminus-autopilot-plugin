5b9499a19595ebac12802c8403fcdaea6f8bd121
Tom Stovall <stovak@gmail.com>
2023-01-26 04:24:54 -0800

    add phpversion to sitename

--- .github/e2e.sh
+++ .github/e2e.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 SCRIPT=$(readlink -f $0)
-SCRIPTPATH=`dirname $SCRIPT`
-ROOT_DIR=`dirname $SCRIPTPATH`
+SCRIPTPATH=$(dirname $SCRIPT)
+ROOT_DIR=$(dirname $SCRIPTPATH)
 VERSION=$(cat .version)
 VERSION_SAFE="${VERSION//./}"
-SITENAME="${VERSION_SAFE}-api-testing"
-CI_ORG_ID=5ae1fa30-8cc4-4894-8ca9-d50628dcba17
+CI_ORG_ID=0238f947-88b4-4b63-b594-343b0fb25641
+PHP_VERSION=$(php -r "echo PHP_MAJOR_VERSION.PHP_MINOR_VERSION;")
+SITENAME="${VERSION_SAFE}-PHP${PHP_VERSION//./}-autopilot"
 
 terminus site:delete "${SITENAME}" --yes --quiet &> /dev/null
 
@@ -15,13 +16,16 @@ echo "Root Dir: ${ROOT_DIR}"
 echo "Version: ${VERSION_SAFE}"
 echo "Testing Site: ${SITENAME}"
 echo "===================================================="
+
 echo "Installing Plugin: "
 terminus self:plugin:install ${ROOT_DIR}
 echo "===================================================="
+
 echo "Creating Site: ${SITENAME}"
 ## If exists is empty, create the site
 terminus site:create "${SITENAME}" "${SITENAME}" drupal9 --org=${CI_ORG_ID}
 echo "===================================================="
+
 echo "Installing Site: ${SITENAME}"
 ## Wipe the site Database and install basic umami
 terminus drush ${SITENAME}.dev -- \
@@ -29,10 +33,12 @@ terminus drush ${SITENAME}.dev -- \
        --site-name=${SITENAME}  \
        --locale=en --yes demo_umami
 echo "===================================================="
+
 echo "Setting Connection: ${SITENAME}"
 ## set the connection of the site to GIT mode
 terminus connection:set ${SITENAME}.dev git
 echo "===================================================="
+
 echo "Activating Autopilot: ${SITENAME}"
 terminus site:autopilot:activate $SITENAME
 echo "===================================================="
@@ -45,18 +51,23 @@ echo "Loading Homepage: ${SITE_DEV}"
 ## curl the page once to make sure you initialize all the database tables
 curl $SITE_DEV &> /dev/null
 echo "===================================================="
+
 echo "Deploy Env's:: ${SITENAME}"
 ## Deploy the dev site to test & live
 terminus env:deploy ${SITENAME}.test && terminus env:deploy ${SITENAME}.live
 echo "===================================================="
+
 echo "Enable Env sync: ${SITENAME}"
 terminus site:autopilot:env-sync:enable $SITENAME
 echo "===================================================="
+
 echo "Enable Env sync: ${SITENAME}"
 terminus site:autopilot:frequency $SITENAME daily
 echo "===================================================="
+
 echo "Setting Frequency: ${SITENAME}"
 terminus site:autopilot:deployment-destination $SITENAME test
 echo "===================================================="
+
 echo "Deleting test site: ${SITENAME}"
 terminus site:delete "${SITENAME}" --yes --quiet
